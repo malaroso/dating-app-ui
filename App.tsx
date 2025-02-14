cd7e893495795914c987222f30ef8106ccaf5f53
Emirhan Ekin <ekinees@hotmail.com>
2025-02-14 18:04:50 +0300

    added new route

--- App.tsx
+++ App.tsx
@@ -3,16 +3,25 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './src/screens/SplashScreen';
 import LoginScreen from './src/screens/LoginScreen';
-
+import PhoneNumberScreen from './src/screens/PhoneNumberScreen';
+import VerificationScreen from './src/screens/VerificationScreen';
+import UserInfoScreen from './src/screens/UserInfoScreen';
+import UserInfoStep2Screen from './src/screens/UserInfoStep2Screen';
 const Stack = createStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash">
         <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="PhoneNumberScreen" component={PhoneNumberScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="VerificationScreen" component={VerificationScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="UserInfoScreen" component={UserInfoScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="UserInfoStep2" component={UserInfoStep2Screen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-} 
+};
+
+export default App; 
