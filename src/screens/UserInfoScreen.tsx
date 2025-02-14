63a910322dd01d6c20933d308a0b582b5743696c
Emirhan Ekin <ekinees@hotmail.com>
2025-02-14 18:04:19 +0300

    Create UserInfoScreen

---
+++ src/screens/UserInfoScreen.tsx
@@ -0,0 +1,216 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, SafeAreaView, Modal, Platform } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { colors } from '../styles/colors';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+import { StackNavigationProp } from '@react-navigation/stack';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+// Stack parametrelerini tanımlayın
+type RootStackParamList = {
+  UserInfoStep2: undefined; // İkinci adıma yönlendirme
+  VerificationScreen: { phoneNumber: string };
+};
+
+type UserInfoScreenNavigationProp = StackNavigationProp<RootStackParamList, 'UserInfoStep2'>;
+
+const UserInfoScreen = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [birthDate, setBirthDate] = useState(new Date());
+  const [tempDate, setTempDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const navigation = useNavigation<UserInfoScreenNavigationProp>();
+  const route = useRoute();
+  const { phoneNumber } = route.params || {}; // Varsayılan değer ekleyin
+
+  const [fontsLoaded] = Font.useFonts({
+    'Montserrat-Medium': require('../../assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-Bold': require('../../assets/fonts/Montserrat-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  const handleContinue = () => {
+    const formData = {
+      firstName,
+      lastName,
+      birthDate,
+      phoneNumber, // Önceki ekrandan gelen telefon numarası
+    };
+
+    navigation.navigate('UserInfoStep2', { formData });
+  };
+
+  const handleNext = () => {
+    navigation.navigate('VerificationScreen', { phoneNumber });
+  };
+
+  const onChange = (event: any, selectedDate?: Date) => {
+    const currentDate = selectedDate || tempDate;
+    setTempDate(currentDate);
+  };
+
+  const handleDone = () => {
+    setBirthDate(tempDate);
+    setShowDatePicker(false);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+        <View style={styles.arrowContainer}>
+          <FontAwesome name="arrow-left" size={20} color={colors.buttonText} />
+        </View>
+      </TouchableOpacity>
+      <View style={styles.innerContainer}>
+        <Text style={styles.title}>Just a few details and you're in! 🚀</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="First Name"
+          value={firstName}
+          onChangeText={setFirstName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Last Name"
+          value={lastName}
+          onChangeText={setLastName}
+        />
+        <TouchableOpacity style={styles.input} onPress={() => setShowDatePicker(true)}>
+          <Text style={styles.inputText}>
+            {birthDate.toLocaleDateString()}
+          </Text>
+        </TouchableOpacity>
+        <Modal
+          visible={showDatePicker}
+          transparent={true}
+          animationType="slide"
+          onRequestClose={() => setShowDatePicker(false)}
+        >
+          <TouchableOpacity style={styles.modalContainer} onPress={() => setShowDatePicker(false)}>
+            <View style={styles.pickerContainer}>
+              <TouchableOpacity style={styles.closeButton} onPress={() => setShowDatePicker(false)}>
+                <FontAwesome name="close" size={24} color={colors.text} />
+              </TouchableOpacity>
+              <DateTimePicker
+                value={tempDate}
+                mode="date"
+                display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                onChange={onChange}
+                maximumDate={new Date()}
+              />
+              <TouchableOpacity style={styles.modalButton} onPress={handleDone}>
+                <Text style={styles.buttonText}>Done</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        </Modal>
+        <TouchableOpacity style={styles.button} onPress={handleContinue}>
+          <Text style={styles.buttonText}>Continue   <FontAwesome name="arrow-right" style={styles.buttonfonts} size={20} color={colors.buttonText} /></Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.background,
+    padding: 20,
+  },
+  backButton: {
+    position: 'absolute',
+    top: 40,
+    left: 20,
+  },
+  arrowContainer: {
+    backgroundColor: colors.primary,
+    borderRadius: 20,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerContainer: {
+    flex: 1,
+    alignItems: 'center',
+    width: '100%',
+    marginTop: 100,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: colors.text,
+    marginBottom: 20,
+    marginHorizontal: 20,
+    fontFamily: 'Montserrat-Medium',
+  },
+  input: {
+    width: '90%',
+    height: 50,
+    borderColor: colors.primary,
+    borderWidth: 1,
+    borderRadius: 25,
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    backgroundColor: colors.inputBackground,
+    fontSize: 16,
+    color: colors.text,
+    justifyContent: 'center',
+  },
+  inputText: {
+    fontSize: 16,
+    color: colors.text,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  pickerContainer: {
+    backgroundColor: colors.inputBackground,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 20,
+  },
+  closeButton: {
+    alignSelf: 'flex-end',
+    marginBottom: 10,
+  },
+  modalButton: {
+    backgroundColor: colors.buttonBackground,
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 30,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: colors.buttonBackground,
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 30,
+    marginBottom: 20,
+    width: '90%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: colors.buttonText,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  buttonfonts: {
+    color: colors.buttonText,
+    fontSize: 16,
+    fontWeight: 'medium',
+  },
+});
+
+export default UserInfoScreen; 
